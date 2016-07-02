b3bae1df2e8a2814d48ffe6c2de43f51ab1f77e3
Jessica Austin <jessica@aus10.org>
2016-07-02 15:42:18 -0800

    pull down availability as well

--- scratch.py
+++ scratch.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-import json
 import os
 import pickle
+import calendar
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
 
 areas_file = "areas.pickle"
 
@@ -45,6 +47,8 @@ areas_with_availability_json_file = "areas_with_availability.json"
 availablility_url = "http://dnr.alaska.gov/projects/cabins/CabinAvailability.cfm"
 possible_months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
+calendar.setfirstweekday(calendar.SUNDAY)
+
 availability = {}
 for area in areas:
 
@@ -58,23 +62,44 @@ for area in areas:
         soup = BeautifulSoup(r.content, "lxml")
 
         # months listed
-        elems = soup.find_all("span", {"class": "boldbodytext"})
-        months = [str(elem.text.strip()) for elem in elems]
-        months = [x for x in months if x in possible_months]
+        month_names = soup.find_all("span", {"class": "boldbodytext"})
+        month_names = [str(elem.text.strip()) for elem in month_names]
+        month_names = [x for x in month_names if x in possible_months]
 
         # availability
-        elems = soup.find_all("pre")
-        assert len(months) == len(elems)
-        availability = dict(zip(months, elems))
+        month_dates = soup.find_all("pre")
 
-        for month in availability:
+        # make sure data makes sense
+        assert len(month_names) == len(month_dates), "month names and dates should line up"
+        assert len(month_names) == 8, "should have 8 months"
+        starting_month = datetime.strptime(month_names[0], "%B").replace(year=datetime.now().year)
+        assert starting_month.month == datetime.now().month, "should start on this month"
+
+        # convert raw data to datetimes
+        months = {}
+        current_month = starting_month
+        for i in range(0, len(month_names)):
+
+            num_days = calendar.monthrange(current_month.year, current_month.month)[1]
+            possible_days = [d for d in range(1, num_days + 1)]
+
+            available_days = re.findall(re.compile('\d\d'), month_dates[i].text)
+            available_days = [int(d) for d in available_days]
+            reserved_days = list(set(possible_days).difference(available_days))
+            assert (len(available_days) + len(reserved_days)) == num_days, \
+                "total number of days should equal number of days in month"
+
+            availability = {date(current_month.year, current_month.month, day): True for day in available_days}
+            availability.update({date(current_month.year, current_month.month, day): False for day in reserved_days})
+
+            months[month_names[i]] = availability
+            current_month = current_month + relativedelta(months=1)
+
+        for month in months:
             print(month)
-            print(availability[month])
+            print(months[month])
 
-        cabin["availability"] = availability
-        
-with open(areas_with_availability_json_file, 'w') as outfile:
-    json.dump(areas, outfile)
+        cabin["availability"] = months
 
 pickle.dump(areas, open( areas_with_availability_file, "wb" ) )
 
